05e57327b38b76202a6bedcfa8d12c24aa18e499
Garavirod <rodrigogarciaavila26@gmail.com>
2020-08-25 15:12:35 -0500

    db config

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 
 // Option 2: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("UserNodes", "root", "garavirod14040", {
+const sequelize = new Sequelize("UserNodes", "root", "Garavirod14040#", {
     host: "localhost",
     dialect: "mysql",
     port: "3306",
